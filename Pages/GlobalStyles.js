135006953efa72974f30472028fda1ee41e332d0
Arun <arun.sarkar@nirnayan.com>
2025-06-18 16:10:50 +0530

    global style

--- Pages/GlobalStyles.js
+++ Pages/GlobalStyles.js
@@ -1,9 +1,24 @@
 import { StyleSheet } from 'react-native';
 
+// theme/lightTheme.js
+export const lightTheme = {
+  background: '#FFFFFF',
+  text: '#000000',
+  inputBackground: '#FFFFFF',
+  inputText: '#000000',
+  placeholder: '#666666',
+  border: '#CCCCCC',
+};
+
+
 const GlobalStyles = StyleSheet.create({
   SafeAreaView: {
     paddingBottom:95,
+    text: {
+      fontSize: 16,
+      color: 'red',
+    },
   },
 });
 
-export default GlobalStyles;
+export default [GlobalStyles, lightTheme];
