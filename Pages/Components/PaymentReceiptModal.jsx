886012338d518df053e6e8c4bbc1de845b89e4c8
Tarun Sana <tarunsanats@gmail.com>
2026-02-25 19:53:29 +0530

    changes

--- Pages/Components/PaymentReceiptModal.jsx
+++ Pages/Components/PaymentReceiptModal.jsx
@@ -7,7 +7,7 @@ import {
   TextInput,
   ScrollView,
   Image,
-  ActivityIndicator,
+  ActivityIndicator, Alert
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import AsyncStorage from "@react-native-async-storage/async-storage";
